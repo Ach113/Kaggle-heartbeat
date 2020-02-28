733d7449e5a3c7f82d61a740f00af99da49dcb17
Ach113 <41070955+Ach113@users.noreply.github.com>
2020-02-28 13:11:22 +0400

    removed parts containing stress test database

--- ecg_autoencoder.py
+++ ecg_autoencoder.py
@@ -3,7 +3,6 @@
 import numpy as np
 import wfdb, random
 
-from sklearn.model_selection import train_test_split
 from tensorflow.keras.layers import Input, Conv1D, MaxPooling1D, UpSampling1D
 from tensorflow.keras import Model
 from tensorflow.keras.optimizers import RMSprop
@@ -14,10 +13,20 @@ import matplotlib.pyplot as plt
 ekg_data = wfdb.rdsamp('100', sampto=300000)[0]
 ekg_data = ekg_data.transpose()[0]
 
-noisy_data = wfdb.rdsamp('118e00', sampto=300000)[0]
-noisy_data = noisy_data.transpose()[0]
-
 samples = 300
+
+def plot_results(true_signal, reconstructed_signal, title):
+    fig, axs = plt.subplots(nrows=3, figsize=(6, 4))
+    for ax in axs:
+        n = random.randrange(len(x_train))
+        ax.plot(true_signal[n], label='real')
+        ax.plot(reconstructed_signal[n], label='reconstructed')
+        ax.plot(reconstructed_signal[n] - true_signal[n], label='error', color='red')
+        handles, labels = ax.get_legend_handles_labels()
+        fig.legend(handles, labels, loc='lower left')
+        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    fig.suptitle(title)
+
 
 def autoencoder(data_in):
     # Encoder:
@@ -38,41 +47,17 @@ def autoencoder(data_in):
 x_train = []
 for i in range(0, ekg_data.shape[0]-samples, samples):
     x_train.append(ekg_data[i:i+samples])
-    
-x_noise = []
-for i in range(0, noisy_data.shape[0]-samples, samples):
-    x_noise.append(noisy_data[i:i+samples])
 
 x_train = np.array(x_train)
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
 x_train = keras.utils.normalize(x_train, axis=1)
 
-x_noise = np.array(x_noise)
-x_noise = x_noise.reshape(x_noise.shape[0], x_noise.shape[1], 1)
-x_noise = keras.utils.normalize(x_noise, axis=1)
-
-train_x, valid_x, train_ground, valid_ground = train_test_split(x_train,
-                                                             x_train, 
-                                                             test_size=0.2, 
-                                                             random_state=13)
-data_in = Input(shape=(train_x.shape[1], 1))
+data_in = Input(shape=(x_train.shape[1], 1))
 model = Model(data_in, autoencoder(data_in))
 model.compile(loss='mean_squared_error', optimizer=RMSprop())
 
 model.fit(x_train, x_train, epochs=10)
 
-n = random.randrange(len(x_train))
 x = model.predict(x_train)
 
-fig, ax = plt.subplots()
-ax.plot(x_train[88], label='real signal')
-ax.plot(x[88], label='reconstructed signal')
-ax.plot(abs(x[88] - x_train[88]), label='error signal', color='red')
-ax.legend()
-
-denoised = model.predict(x_noise)
-
-fig, ax = plt.subplots()
-ax.plot(x_noise[88], label='noisy signal')
-ax.plot(denoised[88], label='denoised signal')
-ax.legend()
+plot_results(x_train, x, title='Comparison of real and reconstructed signals')
